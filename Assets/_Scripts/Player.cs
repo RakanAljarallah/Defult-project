d8324d1de8f340d7e59945ce67457ab2cce54686
Rakan Aljarallah <rakanaljarallah.96@gmail.com>
2023-10-01 08:52:05 +0300

    personaly

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Animator anim;
+    float powerHit = 0f;
+    public bool charged = false;
+
 
     private void Update()
     {
@@ -13,24 +16,34 @@ public class Player : MonoBehaviour
 
     void Attack()
     {
-        Debug.Log(Input.GetKeyDown(KeyCode.Space));
-        if (Input.GetKeyDown(KeyCode.Space))
+        Debug.Log(powerHit);
+
+        if (Input.GetKeyDown(KeyCode.Space) && powerHit == 0)
         {
+            powerHit = 0;
             anim.SetFloat("X", 0);
 
             anim.SetFloat("Y", 1);
+            powerHit = Time.time;
+            charged = true;
+            Debug.Log("charged " + charged);
+
         }
-        else if (Input.GetKeyUp(KeyCode.Space))
+        else if (charged && Time.timeScale > powerHit + 1f)
         {
+            Debug.Log("last hi: " + powerHit);
             anim.SetFloat("X", 1);
 
             anim.SetFloat("Y", 1);
         }
-        else
+        else if (Time.timeScale > powerHit + 2f)
         {
+            Debug.Log("none");
             anim.SetFloat("X", 0);
 
             anim.SetFloat("Y", 0);
+            charged = false;
+            powerHit = 0;
         }
     }
 }
